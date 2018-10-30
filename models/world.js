4ea67b738770137a0d15d9675e49ef6bf5aaeafe
Кенни Петровик <a.taranyuk@i-sys.ru>
2018-10-30 19:26:34 +0500

    changed world model

--- models/world.js
+++ models/world.js
@@ -3,13 +3,10 @@ const mongoose = require("../lib/mongoose");
 const modelName = "world";
 
 const worldSchema = mongoose.Schema({
-  user: String,
-  map: String,
-  time: String,
-  temperature: Number,
-  event: {
-    cataclysm: String,
-  },
+  user: { type: String, unique: true },
+  map: Object,
+  events: [],
+
 });
 
 exports.World = mongoose.model(modelName, worldSchema);
