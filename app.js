3e3359f6157964c5d04f0ee11cde35fc1d1aa8f7
p.filatov@i-sys.ru <p.filatov@i-sys.ru>
2018-11-01 13:20:40 +0400

    [+] Без заголовка Access-Control-Allow-Origin нельзя получить ответ от сервера

--- app.js
+++ app.js
@@ -16,6 +16,13 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// Add headers
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // use "*" to all domains
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
